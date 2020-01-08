80c2c4c08c4070dc63e4ffca381180c9ff361e15
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-01-08 13:51:47 -0500

    done

--- index.js
+++ index.js
@@ -19,22 +19,41 @@ function reverseString(string) {
 }
 
 // Write out a recursive function to see if a word is a palindrome.
-function isPalindrome() {
-  
-
+function isPalindrome(string) {
+  if (string.length > 1) {
+    if (string[0] === string[string.length - 1]) {
+      return isPalindrome(string.substring(1, string.length-1))
+    } else {
+      return false
+    }
+  } else {
+    return true
+  }
 }
 
 // Given an array and an index, write a recursive function to add up the elements of an array.
-function addUpTo() {
-
+function addUpTo(array, index) {
+  if (index >= 1) {
+    return array[index] + addUpTo(array, index-1)
+  } else {
+    return array[index]
+  }
 }
 
 // Write a recursive function to find the largest integer in an array.
-function macOf() {
-
+function maxOf(array) {
+  if (array.length > 1) {
+    return array[0] > maxOf(array.slice(1)) ? array[0] : maxOf(array.slice(1))
+  } else {
+    return array[0]
+  }
 }
 
 // Write out a function to see if an array includes a given element.
-function includesNumber() {
-
+function includesNumber(array, element) {
+  if (array.length >= 1) {
+    return array[0] === element ? true : includesNumber(array.slice(1), element)
+  } else {
+    return false
+  }
 }
